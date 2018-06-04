c4b2027f3dfc933cc29e18e97f04041adadd7cd4
Leva7 <leva181777@gmail.com>
2018-06-05 01:55:20 +0500

    :recycle: Unbind toggle-alert from GNOME Schedule

--- toggle-alert.sh
+++ toggle-alert.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-job_config="$HOME/.gnome/gnome-schedule/crontab/1"
-disabled="command_d=#"
-enabled="command_d="
+enabled="\(.*take-break -a\)"
+disabled="#$enabled"
 icon="/usr/share/icons/Numix-Circle/48/apps/userinfo.svg"
 
-if grep -qF "$disabled" "$job_config"; then
-   sed -i "s/$disabled/$enabled/g" "$job_config"
+if crontab -l | grep -q "$disabled"; then
+   crontab -l | sed "s/$disabled/\1/" | crontab
    notify-send "Alert enabled" "Press Ctrl+F11 to disable" -i "$icon"
 else
-   sed -i "s/$enabled/$disabled/g" "$job_config"
+   crontab -l | sed "s/$enabled/#\1/" | crontab
    notify-send "Alert disabled" "Press Ctrl+F11 to enable" -i "$icon"
 fi
